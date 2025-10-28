86b890d1f338d8f101dd1267f1b6163b70fabeb8
Renato Delgado <rena7o.delgado@gmail.com>
2025-10-27 23:32:24 -0300

    ajuste lesao controller

--- src/controllers/Lesao.controller.ts
+++ src/controllers/Lesao.controller.ts
@@ -7,7 +7,17 @@ const lesaoService = new LesaoService();
 export class LesaoController {
 
     async findAll(req: Request, res: Response) {
-        const lesoes = await LesaoService.findAll();
-        return res.json(lesoes);
+        const lesoes = await lesaoService.findAll();
+        res.json(lesoes);
     }
-}
+
+    async findById(req: Request, res: Response) {
+        const { id } = req.params;
+        const lesao = await lesaoService.findById(Number(id));
+        if (lesao) {
+            res.json(lesao);
+        } else {
+            res.status(404).json({ message: "Lesão não encontrada" });
+        }
+    }
+};
